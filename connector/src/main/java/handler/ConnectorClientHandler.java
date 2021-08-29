89b5dd921a616bfe9a153a1858d0b002c39c62d1
yaoqihao <yaoqihao@xiaomi.com>
2021-08-29 23:28:20 +0800

    init

---
+++ connector/src/main/java/handler/ConnectorClientHandler.java
@@ -0,0 +1,12 @@
+package handler;
+
+import com.google.protobuf.Message;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class ConnectorClientHandler extends SimpleChannelInboundHandler<Message> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
+
+    }
+}
